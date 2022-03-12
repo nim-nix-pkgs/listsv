9991b70d5f3621f7c183348778173066ac4a6d11
hugosenari <hugosenari@gmail.com>
2022-03-12 03:02:45 -0300

    chore: re index listsv

---
+++ flake.nix
@@ -0,0 +1,23 @@
+{
+  description = ''Basic operations on singly and doubly linked lists.'';
+
+  inputs.flakeNimbleLib.owner = "riinr";
+  inputs.flakeNimbleLib.ref   = "master";
+  inputs.flakeNimbleLib.repo  = "nim-flakes-lib";
+  inputs.flakeNimbleLib.type  = "github";
+  inputs.flakeNimbleLib.inputs.nixpkgs.follows = "nixpkgs";
+  
+  inputs."listsv-master".url = "path:./master";
+  inputs."listsv-master".inputs.nixpkgs.follows = "nixpkgs";
+  inputs."listsv-master".inputs.flakeNimbleLib.follows = "flakeNimbleLib";
+  
+  outputs = { self, nixpkgs, flakeNimbleLib, ...}@inputs:
+  let 
+    lib  = flakeNimbleLib.lib;
+    args = ["self" "nixpkgs" "flakeNimbleLib"];
+  in lib.mkProjectOutput {
+    inherit self nixpkgs;
+    meta = builtins.fromJSON (builtins.readFile ./meta.json);
+    refs = builtins.removeAttrs inputs args;
+  };
+}
